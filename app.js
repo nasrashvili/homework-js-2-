9029abbee315177d9c19ac450b35ed7be877b1e4
nasrashvili <Nasrashvililuka@gmail.com>
2023-04-10 15:48:31 +0400

    homework js 2

---
+++ app.js
@@ -0,0 +1,61 @@
+  // number 1 
+  const numberArrays = [2,4,6,8,10,12];
+ console.log(numberArrays);
+const lestel = numberArrays.pop(5);
+console.log(numberArrays);
+const pushnumber = numberArrays.unshift(12);
+console.log(numberArrays);
+
+
+// number 2 ;
+//const myarr = [{ productPrice}];
+
+ 
+//const myarr1 =[{ productTitle }];
+   
+//const myarr2 =["pice 25 gel "];
+
+//number 3 
+
+let num1= [5, 7, 9,11, 13]; 
+
+
+console.log(5 + 7 + 9 + 11 + 13 /5 );
+
+// number 4 
+
+let  number4= [
+    {
+        phoneName: "redmi note 11",
+        phoneDescription: "storage 12 GB , camera 50MP ,",
+        phonePrice : 900 ,
+        shopAddresses:"tbilisi ",
+        phoneMemoryInGB:   "128GB" ,
+       
+   },
+];  
+console.log(number4);
+
+// number 5 
+
+const string1 = [` "Phone name is  ${redminote11} phone price is ${900}  and you can buy it at  ${
+	tbilisi -zummer
+
+} `
+];
+
+console.log( 
+     	"phone name is  " +
+    	 +   redminote11 +
+    		"phone price is " +
+    		 gel +
+    		"  and you can buy it at " +
+    		(tbilisi-zummer) 
+    	
+    );
+    // ბოლო დავალება რაღაც ვერ გავიგე კარგად და კომენტარებში დამიწერეთ რაა  მადლობაა )))
+
+    
+
+
+    
